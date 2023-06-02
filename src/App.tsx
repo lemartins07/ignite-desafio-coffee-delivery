427f6502e4062e89affa09d8a9d6ba4524199166
Leandro Martins <leandro.marttins.85@gmail.com>
2023-06-02 19:23:26 -0300

    Estrutura de rotas e páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1 className="title-xl">Title XL</h1>
-      <h2 className="title-l">Title L</h2>
-      <h3 className="title-m">Title M</h3>
-
-      <p className="text-l">Text L</p>
-      <p className="text-m">Text M</p>
-      <p className="text-s">Text S</p>
-      <p className="text-xs">Text XS</p>
-      <input />
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
